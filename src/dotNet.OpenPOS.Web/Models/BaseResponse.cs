093975af4d708d950e7c9e64a791be13babbbf77
M3LiNdRu <rogercalaf@gmail.com>
2016-10-12 22:45:23 +0100

    Add project files.

---
+++ src/dotNet.OpenPOS.Web/Models/BaseResponse.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace dotNet.OpenPOS.Web.Models
+{
+    public class BaseResponse
+    {
+        public BaseResponse(bool success, object data, string message)
+        {
+            Success = success;
+            Data = data;
+            Message = message;
+        }
+
+        public bool Success { get; set; }
+        public object Data { get; set; }
+        public string Message { get; set; }
+    }
+}
